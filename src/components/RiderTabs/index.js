509dbfa11805b00fb911e1d9cf42f138ebd23910
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-01-03 01:35:12 +0500

    Update index.js

--- src/components/RiderTabs/index.js
+++ src/components/RiderTabs/index.js
@@ -3,15 +3,15 @@ import { View, Text, TouchableOpacity, ScrollView, StyleSheet } from 'react-nati
 
 const RiderTabs = ({ tabs, active, onTabChange }) => (
     <View style={styles.container}>
-        <ScrollView
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
+        <View
+            // horizontal={true}
+            // showsHorizontalScrollIndicator={false}
             style={{ flexDirection: 'row' }}>
             {
                 tabs.map((tab, i) => (
                     <React.Fragment key={i}>
                         <TouchableOpacity
-                            style={tabs.length == 2 ? [styles.innerContainer, { paddingHorizontal: 35, backgroundColor: i === active ? '#F8F8F8' : 'white' }] : [styles.innerContainer, { marginHorizontal: 10 }]}
+                            style={tabs.length == 2 ? [styles.innerContainer, { paddingHorizontal: 35, height: 60, backgroundColor: i === active ? '#F8F8F8' : 'white' }] : [styles.innerContainer, { marginHorizontal: 10 }]}
                             onPress={() => onTabChange(i)}>
                             <Text style={{ fontFamily: "Roboto-Medium", fontSize: 14, color: i === active ? '#29B1DB' : '#7A7A7A' }}>{tab}</Text>
                         </TouchableOpacity>
@@ -22,7 +22,7 @@ const RiderTabs = ({ tabs, active, onTabChange }) => (
                     </React.Fragment>
                 ))
             }
-        </ScrollView>
+        </View>
     </View>
 );
 const styles = StyleSheet.create({
